3bfd2e329352a234b17fe40c74e6ae0e09ef3f7e
vt <vtochitsky@gmail.com>
2025-10-13 23:14:12 +0300

    C02 done

--- CH02/02_06/02_06-challenge1.c
+++ CH02/02_06/02_06-challenge1.c
@@ -2,6 +2,8 @@
 
 int main()
 {
+	int a = 1;
+	const char b = 'c';
 	printf("Here is the value of A: %d\n",a);
 	printf("Here is the value of B: %c\n",b);
 
--- CH02/02_13/02_13-challenge1.c
+++ CH02/02_13/02_13-challenge1.c
@@ -3,19 +3,19 @@
 
 int main()
 {
-	//declare constant ratio equal to 3/4
-	//declare char a
-	//declare integer b
-	//declare float c
+	const double ratio = 3.0/4; //declare constant ratio equal to 3/4
+	char a; //declare char a
+	int b; //declare integer b
+	float c; //declare float c
 
-	//assign char
-	//assign integer
-	//assign float
+	a = 'a'; //assign char
+	b = 5; //assign integer
+	c = 0.1f; //assign float
 
-	//output char value, e.g., "the value of variable a is '?'"
-	//output integer value, e.g., "the value of variable b is ??"
-	//output float value, e.g., "the value of variable c is ???.??"
-	//output value of ratio, e.g., "the value of constant ratio is ???.??"
+	printf("the value of variable a is '%c'\n", a);					 // output char value, e.g., "the value of variable a is '?'"
+	printf("the value of variable b is %2i\n", b);					 // output integer value, e.g., "the value of variable b is ??"
+	printf("the value of variable c is %3.2f\n", c);				 // output float value, e.g., "the value of variable c is ???.??"
+	printf("the value of constant ratio is %3.2f\n", ratio); // output value of ratio, e.g., "the value of constant ratio is ???.??"
 
 	return 0;
 }
