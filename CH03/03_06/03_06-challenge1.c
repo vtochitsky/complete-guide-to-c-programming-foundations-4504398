3a6eadfb4b6cf680429314a24a85bb808c680253
vt <vtochitsky@gmail.com>
2025-10-13 23:53:52 +0300

    CH03 done

--- CH03/03_06/03_06-challenge1.c
+++ CH03/03_06/03_06-challenge1.c
@@ -2,11 +2,16 @@
 
 int main()
 {
-	int artifacts,rooms,paintings,sculptures;
+	int artifacts, rooms, paintings, sculptures;
+
+	rooms = 12;
+	paintings = 16;
+	sculptures = 4;
 
 	/* code your calculations here */
+	artifacts = rooms * (paintings + sculptures);
 
-	printf("This museum has %d artifacts\n",artifacts);
+	printf("This museum has %d artifacts\n", artifacts);
 
-	return(0);
+	return (0);
 }
--- CH03/03_13/03_13-challenge1.c
+++ CH03/03_13/03_13-challenge1.c
@@ -9,12 +9,19 @@ int main()
 	scanf("%d",&b);
 
 	// add
+	printf("%i + %i = %i\n", a, b, a+b);
 	// subtract
+	printf("%i - %i = %i\n", a, b, a-b);
 	// multiply
+	printf("%i * %i = %i\n", a, b, a*b);
 	// divide
+	printf("%i / %i = %f\n", a, b, (double)a/b);
 	// modulus
+	printf("%i %% %i = %d\n", a, b, a % b);
 	// bit shift right
+	printf("%i >> %i = %i\n", a, b, a >> b);
 	// bit shift left
+	printf("%i << %i = %i\n", a, b, a << b);
 
 	return 0;
 }
